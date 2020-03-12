77d8e05bf7472ae66d14fe99a85ab3c2df1a94e3
Mathias Koch <mk@blackbird.online>
2020-03-12 11:45:24 +0100

    Fix Cortex-m-rt example

--- atat/examples/cortex-m-rt.rs
+++ atat/examples/cortex-m-rt.rs
@@ -1,127 +1,115 @@
-// // #![deny(warnings)]
-// #![no_main]
-// #![no_std]
+#![no_main]
+#![no_std]
+// TEMP: Crate wide allow this
+#![allow(deprecated, unused)]
 
-// extern crate atat;
-// extern crate cortex_m;
-// extern crate cortex_m_rt as rt;
-// extern crate heapless;
-// extern crate nb;
-// extern crate panic_halt;
-// extern crate stm32l4xx_hal as hal;
+extern crate atat;
+extern crate cortex_m;
+extern crate cortex_m_rt as rt;
+extern crate heapless;
+extern crate nb;
+extern crate panic_halt;
+extern crate stm32l4xx_hal as hal;
 
-// mod common;
-// use common::command::{Command, Response};
+mod common;
 
-// use crate::hal::{
-//     gpio::{
-//         gpioa::{PA2, PA3},
-//         Alternate, Floating, Input, AF7,
-//     },
-//     prelude::*,
-//     serial::{Config, Rx, Serial},
-//     stm32::{self, interrupt},
-//     timer::{Event, Timer},
-// };
+use cortex_m::asm;
+use hal::{
+    gpio::{
+        gpioa::{PA2, PA3},
+        Alternate, Floating, Input, AF7,
+    },
+    pac::{interrupt, Peripherals, USART2},
+    prelude::*,
+    serial::{Config, Event::Rxne, Rx, Serial},
+    timer::{Event, Timer},
+};
 
-// use crate::rt::entry;
-// use cortex_m::asm;
-// use heapless::{consts, spsc::Queue};
+use atat::prelude::*;
 
-// use atat::ATParser;
-// use atat::Error as ATError;
+use heapless::{consts, spsc::Queue, String};
 
-// type SerialUSART2 = Serial<
-//     stm32::USART2,
-//     (
-//         PA2<Alternate<AF7, Input<Floating>>>,
-//         PA3<Alternate<AF7, Input<Floating>>>,
-//     ),
-// >;
+use crate::rt::entry;
 
-// static mut REQ_Q: Option<Queue<Command, consts::U5, u8>> = None;
-// static mut RES_Q: Option<Queue<Result<Response, ATError>, consts::U5, u8>> = None;
-// static mut AT_PARSER: Option<ATParser<Rx<stm32::USART2>, consts::U64>> = None;
+static mut INGRESS: Option<atat::IngressManager> = None;
+static mut RX: Option<Rx<USART2>> = None;
 
-// #[entry]
-// fn main() -> ! {
-//     let p = stm32::Peripherals::take().unwrap();
+#[entry]
+fn main() -> ! {
+    let p = Peripherals::take().unwrap();
 
-//     let mut flash = p.FLASH.constrain();
-//     let mut rcc = p.RCC.constrain();
+    let mut flash = p.FLASH.constrain();
+    let mut rcc = p.RCC.constrain();
+    let mut pwr = p.PWR.constrain(&mut rcc.apb1r1);
 
-//     let mut gpioa = p.GPIOA.split(&mut rcc.ahb2);
-//     // let mut gpiob = p.GPIOB.split(&mut rcc.ahb2);
+    let mut gpioa = p.GPIOA.split(&mut rcc.ahb2);
+    // let mut gpiob = p.GPIOB.split(&mut rcc.ahb2);
 
-//     // clock configuration using the default settings (all clocks run at 8 MHz)
-//     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-//     // TRY this alternate clock configuration (clocks run at nearly the maximum frequency)
-//     // let clocks = rcc.cfgr.sysclk(64.mhz()).pclk1(32.mhz()).freeze(&mut flash.acr);
+    // clock configuration using the default settings (all clocks run at 8 MHz)
+    let clocks = rcc.cfgr.freeze(&mut flash.acr, &mut pwr);
+    // TRY this alternate clock configuration (clocks run at nearly the maximum frequency)
+    // let clocks = rcc.cfgr.sysclk(64.mhz()).pclk1(32.mhz()).freeze(&mut flash.acr);
 
-//     // The Serial API is highly generic
-//     // TRY the commented out, different pin configurations
-//     // let tx = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
-//     // let tx = gpiob.pb6.into_af7(&mut gpiob.moder, &mut gpiob.afrl);
+    // The Serial API is highly generic
+    // TRY the commented out, different pin configurations
+    // let tx = gpioa.pa9.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
+    // let tx = gpiob.pb6.into_af7(&mut gpiob.moder, &mut gpiob.afrl);
 
-//     // let rx = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
-//     // let rx = gpiob.pb7.into_af7(&mut gpiob.moder, &mut gpiob.afrl);
+    // let rx = gpioa.pa10.into_af7(&mut gpioa.moder, &mut gpioa.afrh);
+    // let rx = gpiob.pb7.into_af7(&mut gpiob.moder, &mut gpiob.afrl);
 
-//     let tx = gpioa.pa2.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
-//     let rx = gpioa.pa3.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
+    let tx = gpioa.pa2.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
+    let rx = gpioa.pa3.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
 
-//     unsafe { REQ_Q = Some(Queue::u8()) };
-//     unsafe { RES_Q = Some(Queue::u8()) };
+    let mut timer = Timer::tim7(p.TIM7, 1.hz(), clocks, &mut rcc.apb1r1);
+    let at_timer = Timer::tim6(p.TIM6, 100.hz(), clocks, &mut rcc.apb1r1);
 
-//     // TRY using a different USART peripheral here
-//     // let serial = Serial::usart1(p.USART1, (tx, rx), 9_600.bps(), clocks, &mut rcc.apb2);
-//     let serial = Serial::usart2(
-//         p.USART2,
-//         (tx, rx),
-//         Config::default().baudrate(115_200.bps()),
-//         clocks,
-//         &mut rcc.apb1r1,
-//     );
-//     let tim = Timer::tim6(p.TIM6, 100.hz(), clocks, &mut rcc.apb1r1);
+    let mut serial = Serial::usart2(
+        p.USART2,
+        (tx, rx),
+        Config::default().baudrate(115_200.bps()),
+        clocks,
+        &mut rcc.apb1r1,
+    );
 
-//     let (at_client, at_parser) = atat::new(
-//         unsafe { (REQ_Q.as_mut().unwrap(), RES_Q.as_mut().unwrap()) },
-//         serial.split(),
-//         tim,
-//         1.hz(),
-//     );
+    serial.listen(Rxne);
 
-//     let (mut cmd_p, mut resp_c) = at_client.release();
+    let (tx, rx) = serial.split();
+    let (mut client, ingress) = atat::new(tx, at_timer, atat::Config::new(atat::Mode::Timeout));
 
-//     unsafe { AT_PARSER = Some(at_parser) };
-//     // configure NVIC interrupts
-//     unsafe { cortex_m::peripheral::NVIC::unmask(hal::stm32::Interrupt::TIM7) };
+    unsafe { INGRESS = Some(ingress) };
+    unsafe { RX = Some(rx) };
 
-//     // Adjust this spin rate to set how often the request/response queue is checked
-//     let mut timer = Timer::tim7(p.TIM7, 100.hz(), clocks, &mut rcc.apb1r1);
-//     timer.listen(Event::TimeOut);
+    // configure NVIC interrupts
+    unsafe { cortex_m::peripheral::NVIC::unmask(hal::stm32::Interrupt::TIM7) };
+    timer.listen(Event::TimeOut);
 
-//     // if all goes well you should reach this breakpoint
-//     asm::bkpt();
+    // if all goes well you should reach this breakpoint
+    asm::bkpt();
 
-//     cmd_p.enqueue(Command::AT).unwrap();
+    loop {
+        asm::wfi();
 
-//     loop {
-//         asm::wfi();
+        match client.send(&common::AT) {
+            Ok(response) => {
+                // Do something with response here
+            }
+            Err(e) => {}
+        }
+    }
+}
 
-//         if let Some(_response) = resp_c.dequeue() {
-//             // Do something with response here
-//         }
-//     }
-// }
+#[interrupt]
+fn TIM7() {
+    let ingress = unsafe { INGRESS.as_mut().unwrap() };
+    ingress.parse_at();
+}
 
-// #[interrupt]
-// fn TIM7() {
-//     let at_parser = unsafe { AT_PARSER.as_mut().unwrap() };
-//     at_parser.spin();
-// }
-
-// #[interrupt]
-// fn USART1() {
-//     let at_parser = unsafe { AT_PARSER.as_mut().unwrap() };
-//     at_parser.handle_irq();
-// }
+#[interrupt]
+fn USART2() {
+    let ingress = unsafe { INGRESS.as_mut().unwrap() };
+    let rx = unsafe { RX.as_mut().unwrap() };
+    if let Ok(d) = nb::block!(rx.read()) {
+        ingress.write(&[d]);
+    }
+}
