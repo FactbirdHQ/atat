f6b949bfa01bfe68f76a1d2666d1d1b477c7b242
Danilo Bargen <mail@dbrgn.ch>
2020-03-13 15:26:04 +0100

    Add some trait docs (#21)

--- atat/src/traits.rs
+++ atat/src/traits.rs
@@ -4,6 +4,7 @@ use heapless::{ArrayLength, String};
 
 pub trait ATATErr {}
 
+/// This trait needs to be implemented for every response type.
 pub trait ATATResp {}
 
 pub trait ATATUrc {
@@ -12,18 +13,32 @@ pub trait ATATUrc {
     fn parse(resp: &str) -> Result<Self::Resp>;
 }
 
+/// This trait needs to be implemented for every command type.
 pub trait ATATCmd {
-    type Response: ATATResp;
+    /// The max length of the command.
+    ///
+    /// Example: For the command "AT+RST" you would specify
+    ///
+    /// ```
+    /// type CommandLen = heapless::consts::U6;
+    /// ```
     type CommandLen: ArrayLength<u8>;
 
+    /// The type of the response. Must implement the `ATATResp` trait.
+    type Response: ATATResp;
+
+    /// Return the command as a heapless `String`.
     fn as_str(&self) -> String<Self::CommandLen>;
 
+    /// Parse the string response into a `Self::Response` instance.
     fn parse(&self, resp: &str) -> Result<Self::Response>;
 
+    /// Whether or not this command can be aborted.
     fn can_abort(&self) -> bool {
         false
     }
 
+    /// The max timeout in milliseconds.
     fn max_timeout_ms(&self) -> u32 {
         1000
     }
