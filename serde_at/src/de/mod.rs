7db20a8b6660f3b048b9182470325621238939b3
unizippro <mads@astrupa.dk>
2020-11-13 11:17:26 +0100

    new() for CharVec

--- serde_at/src/de/mod.rs
+++ serde_at/src/de/mod.rs
@@ -45,6 +45,13 @@ pub type Result<T> = core::result::Result<T, Error>;
 #[derive(Debug, PartialEq)]
 pub struct CharVec<T: heapless::ArrayLength<char>>(heapless::Vec<char, T>);
 
+impl <T> CharVec<T>
+    where T: heapless::ArrayLength<char>{
+    pub fn new() -> Self{
+        CharVec(heapless::Vec::<char, T>::new())
+    }
+}
+
 impl<'de, N> Deserialize<'de> for CharVec<N>
 where
     N: heapless::ArrayLength<char>,
@@ -826,6 +833,8 @@ mod tests {
 
     #[test]
     fn char_vec_struct() {
+        let expectation: CharVec<consts::U4> = CharVec::new();
+        assert_eq!(CharVec(heapless::Vec::<char, consts::U4>::new()), expectation);
         let expectation: CharVec<consts::U4> =
             CharVec(heapless::Vec::from_slice(&['I', 'M', 'P', '_']).unwrap());
         assert_eq!(crate::from_str("+CCID: IMP_"), Ok(expectation));
