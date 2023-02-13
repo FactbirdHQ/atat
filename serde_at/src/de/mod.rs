4ff93379d4c5e0c4b1ab11ef3ba2b23483d0bdfb
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-02-13 16:00:04 +0100

    Make clippy happy on nightly

--- serde_at/src/de/mod.rs
+++ serde_at/src/de/mod.rs
@@ -673,12 +673,12 @@ impl fmt::Display for Error {
 }
 
 fn trim_ascii_whitespace(x: &[u8]) -> &[u8] {
-    let from = match x.iter().position(|x| !x.is_ascii_whitespace()) {
-        Some(i) => i,
-        None => return &x[0..0],
-    };
-    let to = x.iter().rposition(|x| !x.is_ascii_whitespace()).unwrap();
-    &x[from..=to]
+    if let Some(from) = x.iter().position(|x| !x.is_ascii_whitespace()) {
+        let to = x.iter().rposition(|x| !x.is_ascii_whitespace()).unwrap();
+        &x[from..=to]
+    } else {
+        &x[0..0]
+    }
 }
 
 /// Deserializes an instance of type `T` from bytes of AT Response text
