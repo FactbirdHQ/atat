028766aa81494c3f685da8d956c840c981936ed4
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2023-07-06 10:36:37 +0200

    Let Urc Subscriber be Send (#159)
    Fixes 152

--- atat/src/urc_channel.rs
+++ atat/src/urc_channel.rs
@@ -1,11 +1,12 @@
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use embassy_sync::pubsub::{DynSubscriber, PubSubBehavior, PubSubChannel, Publisher};
+use embassy_sync::pubsub::{PubSubBehavior, PubSubChannel, Publisher, Subscriber};
 
 use crate::AtatUrc;
 
 pub type UrcPublisher<'sub, Urc, const CAPACITY: usize, const SUBSCRIBERS: usize> =
     Publisher<'sub, CriticalSectionRawMutex, <Urc as AtatUrc>::Response, CAPACITY, SUBSCRIBERS, 1>;
-pub type UrcSubscription<'sub, Urc> = DynSubscriber<'sub, <Urc as AtatUrc>::Response>;
+pub type UrcSubscription<'sub, Urc, const CAPACITY: usize, const SUBSCRIBERS: usize> =
+    Subscriber<'sub, CriticalSectionRawMutex, <Urc as AtatUrc>::Response, CAPACITY, SUBSCRIBERS, 1>;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
@@ -13,8 +14,8 @@ pub enum Error {
     MaximumSubscribersReached,
 }
 
-pub trait AtatUrcChannel<Urc: AtatUrc> {
-    fn subscribe(&self) -> Result<UrcSubscription<'_, Urc>, Error>;
+pub trait AtatUrcChannel<Urc: AtatUrc, const CAPACITY: usize, const SUBSCRIBERS: usize> {
+    fn subscribe(&self) -> Result<UrcSubscription<'_, Urc, CAPACITY, SUBSCRIBERS>, Error>;
     fn space(&self) -> usize;
 }
 
@@ -34,12 +35,12 @@ impl<Urc: AtatUrc, const CAPACITY: usize, const SUBSCRIBERS: usize>
     }
 }
 
-impl<Urc: AtatUrc, const CAPACITY: usize, const SUBSCRIBERS: usize> AtatUrcChannel<Urc>
-    for UrcChannel<Urc, CAPACITY, SUBSCRIBERS>
+impl<Urc: AtatUrc, const CAPACITY: usize, const SUBSCRIBERS: usize>
+    AtatUrcChannel<Urc, CAPACITY, SUBSCRIBERS> for UrcChannel<Urc, CAPACITY, SUBSCRIBERS>
 {
-    fn subscribe(&self) -> Result<UrcSubscription<'_, Urc>, Error> {
+    fn subscribe(&self) -> Result<UrcSubscription<'_, Urc, CAPACITY, SUBSCRIBERS>, Error> {
         self.0
-            .dyn_subscriber()
+            .subscriber()
             .map_err(|_| Error::MaximumSubscribersReached)
     }
 
